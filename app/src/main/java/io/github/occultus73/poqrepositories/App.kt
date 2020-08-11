800d393dd7f9a51bee5f478883032452f0582352
John O. A. Rayner-Hilles <john.rayner-hilles@appexperts.me.uk>
2020-08-11 11:10:15 +0100

    Initial commit

---
+++ app/src/main/java/io/github/occultus73/poqrepositories/App.kt
@@ -0,0 +1,7 @@
+package io.github.occultus73.poqrepositories
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
---
+++ app/src/main/java/io/github/occultus73/poqrepositories/ui/MainActivity.kt
@@ -0,0 +1,7 @@
+package io.github.occultus73.poqrepositories.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.occultus73.poqrepositories.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
